551a62fd2536b9f44776267385ccfcdc6dacf645
Minseok Bang <minseokbang@Minseokui-MacBook-Air.local>
2018-10-19 00:52:36 +0900

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -22,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
