f6417d68277801d772b1acf39fbf4bec76288245
Minseok Bang <minseokbang@Minseokui-MacBook-Air.local>
2018-10-19 11:08:16 +0900

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
@@ -27,9 +28,9 @@ Rails.application.configure do
   config.assets.compress = true
   # Generate digests for assets URLs  
   config.assets.digest = true
-  
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
