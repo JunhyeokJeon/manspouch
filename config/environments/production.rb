eee86073f3ec7df988985dcddd6ebf57b86ceb10
Minseok Bang <minseokbang@Minseokui-MacBook-Air.local>
2018-10-19 11:48:53 +0900

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,14 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS  
   config.assets.compress = true
-  # Generate digests for assets URLs  
-  config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
+  # Generate digests for assets URLs  
+  config.assets.digest = true
+
+ 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
